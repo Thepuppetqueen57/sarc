4559dee19a60980bc9c486a02a41c22bd6511c4b
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-04-09 11:43:20 -0700

    Does not work yet but yeah
    Idk why this doesn't work

--- src/parser.rs
+++ src/parser.rs
@@ -21,6 +21,31 @@ pub fn parse_code(code: String) {
         let why_me_why_me: usize = func_args.len();
         func_args[why_me_why_me - 1].pop();
 
+        let func_name: String = func_args[0].clone();
+        let arguments_str: String = func_args[1].clone();
+        let arguments: Vec<&str> = arguments_str.split(", ").collect();
+
+        if func_name == "print" {
+            let mut str_to_print: String = "".to_string();
+            let mut reading_str: bool = false;
+
+            for char in arguments[0].chars() {
+                if char == '"' {
+                    if reading_str {
+                        reading_str = false
+                    } else {
+                        reading_str = true
+                    }
+                }
+
+                if reading_str {
+                    str_to_print.push(char);
+                }
+            }
+
+            println!("{}", str_to_print);
+        }
+
         index += 1
     }
 }
